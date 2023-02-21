b338effec027696b62b94b904230e5edf33188af
nazwaAuliaa <110008117+nazwaAuliaa@users.noreply.github.com>
2023-02-21 09:00:40 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$id=$_GET["id_pengaduan"];
-$db=new PDO("mysql:host=localhost;dbname=pengaduanmasyarakat",'root','');
-$query=$db->query("DELETE FROM `pengaduan` WHERE id_pengaduan='$id'");
-if($query){
-    header('location:masyarakat.php');
-}
